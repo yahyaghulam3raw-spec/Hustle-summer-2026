69494f0aeb0e37ced074691797523db9c1d9af66
Yahya Noorzai <yahyaghulam3raw@gmail.com>
2026-06-16 11:24:49 -0700

    week 2 lab assigmnet

---
+++ Profile.py
@@ -0,0 +1,13 @@
+fuill_name = "Yahya Noorzai"
+nickname = "yaya"
+age = 17
+used_python_before = False
+hobbies= ["fishing", "hiking","sleeping"]
+favorites = {"movie": "spiderman",
+ "food": "rice", 
+ "hobbies": "fishing"
+ } 
+print(fuill_name, age,hobbies, favorites)
+skills = ("eating", "swimming") #tuple: ordered, cannot change 
+language_spoke = {"English", "Pashto"}
+print(skills, language_spoke)
